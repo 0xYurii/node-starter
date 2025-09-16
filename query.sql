29523bd4f47d6ad665bb0070e855e48c0f7140b6
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-09-16 22:40:38 +0100

    use OR operator

--- query.sql
+++ query.sql
@@ -1,5 +1,9 @@
 /*
-	Select cars made between 1980 and 1989
-		show the brand, model, year and price
+	Search for brand, model, color, year and price of cars
+		where the color is a shade of red
+		or the year is between 1960 and 1969
 */
-
+SELECT brand, model, color, year, price, sold FROM cars
+	WHERE (color LIKE '%red%'
+	OR year BETWEEN 1960 AND 1969)
+	AND sold='false';

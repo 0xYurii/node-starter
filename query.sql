aebbce31d14e19646c6edafd05498a377de12083
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-09-17 10:09:38 +0100

    use MAX, MIN, AVG opertator

--- query.sql
+++ query.sql
@@ -1,8 +1,10 @@
 /*
-	Sum the price of cars
+	Select the average, minimum and maximum price from cars
 		where sold is true
-	Use the alias  in your output
+	Round the average up to the nearest whole number
+		and use 'avg' as the alias for that result	
 */
 
-SELECT SUM(price) AS total_earnings FROM cars
+
+SELECT FLOOR (AVG(price)) AS avertage_price, MIN(price) AS min_price, MAX(price) AS max_price FROM cars
 	WHERE sold is TRUE;

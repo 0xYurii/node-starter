5ee31d50b19e1d698f3f6a9cb968a8a2019fa429
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-09-19 11:09:03 +0100

    ALL

--- query.sql
+++ query.sql
@@ -1,11 +1,17 @@
 /*
-	Select the brand, model and price where
-		* the price is lower than any Ford
-		* the brand is 
-*/ 
+	Select the brand, model and price from cars
+	Where the price of the car is greater
+		than the total sales of any dealership
+*/
+
+
+
+
 
 SELECT brand, model, price FROM cars 
-WHERE price < ANY (
-	SELECT price FROM cars 
-	WHERE brand = 'Ford'
-) AND brand = 'Volkswagen';
+	WHERE price > ANY (
+	SELECT SUM(sold_price) FROM sold_cars
+	JOIN staff ON sold_cars.seller=staff.id
+	GROUP BY staff.dealership_id
+);
+

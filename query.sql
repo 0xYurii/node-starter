749f218df9e5377904c261646422428e131f072e
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-09-19 11:08:55 +0100

    ANY

--- query.sql
+++ query.sql
@@ -1,24 +1,11 @@
 /*
-	Show the city and state of dealerships
-		with a count of the cars sold
-		aliased as cars_sold
-		
-	Select from sold_cars
-		join with the relevant tables
-		
-	Include dealerships which have no sold cars
-	
-	Order the count in descending order
-		
-	Hint: you may need to join using a table not included in our columns
-*/
+	Select the brand, model and price where
+		* the price is lower than any Ford
+		* the brand is 
+*/ 
 
-SELECT
-	D.city,
-	D.state,
-	COUNT(SC.id) AS cars_sold
-FROM sold_cars SC
-	LEFT JOIN cars C ON SC.cars_id = C.id
-	RIGHT JOIN dealerships D ON C.dealership_id = D.id
-GROUP BY D.city, D.state
-ORDER BY cars_sold DESC;
+SELECT brand, model, price FROM cars 
+WHERE price < ANY (
+	SELECT price FROM cars 
+	WHERE brand = 'Ford'
+) AND brand = 'Volkswagen';

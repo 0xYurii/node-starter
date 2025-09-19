06eccfe4a46fb379b9ab24f93d039d8933d32e86
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-09-19 14:00:59 +0100

    Refactor SQL query to select salespeople who haven't sold cars over $45,000

--- query.sql
+++ query.sql
@@ -1,18 +1,12 @@
 /*
-	Select the brand, model and price from cars
-	Where the price of the car is greater
-		than the total sales of any dealership
+	Select the name of salespeople
+		(role = '')
+	who have not sold a car for more than $45,000
 */
 
-
-
-
-
-SELECT brand, model, price FROM cars 
-	WHERE price > ANY (
-	SELECT SUM(sold_price) FROM sold_cars
-	JOIN staff ON sold_cars.seller=staff.id
-	GROUP BY staff.dealership_id
-);
-
-
+SELECT name FROM staff S
+WHERE NOT EXISTS(
+	SELECT 1 FROM sold_cars SC
+	WHERE sold_price > 45000 AND SC.seller=S.id
+)AND role= 'Salesperson'
+AND EXISTS (SELECT 1 FROM sold_cars SC where seller = s.id)
